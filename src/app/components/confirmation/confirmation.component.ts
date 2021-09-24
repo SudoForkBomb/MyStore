d8d1db8dcd36345506cb960493c91aa14cf88725
Chris Taylor <chrisrtaylor123@gmail.com>
2021-09-24 12:14:50 -0800

    feat: implemented confirmation page after order is submitted

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { OrderInfo } from 'src/app/models/orderInfo';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.css']
+  styleUrls: ['./confirmation.component.css'],
 })
 export class ConfirmationComponent implements OnInit {
+  orderInfo!: OrderInfo;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.orderInfo = this.cartService.getOrderInfo();
   }
 
+  ngOnDestroy(): void {
+    this.cartService.setOrderInfo(new OrderInfo());
+  }
 }
